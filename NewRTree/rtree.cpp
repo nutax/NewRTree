ff91051a6b656f9eab39ba941e7ced95fb59ef55
nutax <jose.huby@utec.edu.pe>
2022-07-10 23:30:45 -0500

    insert 90%

--- NewRTree/rtree.cpp
+++ NewRTree/rtree.cpp
@@ -8,11 +8,32 @@ RTree::RTree()
 
 RTree::~RTree()
 {
+	if(_root != nullptr) freeMemory(_root);
+}
+
+void RTree::freeMemory(Node* current)
+{
+	if (current->leaf) {
+		for (auto& mbb : current->regions) {
+			delete mbb.child;
+		}
+	}
+	else {
+		for (auto& mbb : current->regions) {
+			freeMemory((Node*)(mbb.child));
+		}
+	}
+	delete current;
 }
 
 void RTree::insert(Poly const& poly)
 {
 	insertHelper(buildMBB(poly));
+}
+
+void RTree::print()
+{
+
 }
 
 MBB RTree::buildMBB(Poly const& poly)
@@ -28,31 +49,31 @@ MBB RTree::buildMBB(Poly const& poly)
 	return mbb;
 }
 
-void RTree::insertHelper(MBB mbb)
+void RTree::insertHelper(MBB newMBB)
 {
 	_size++;
 	if (_root == nullptr) {
 		_root = new Node;
-		_root->regions.push_back(mbb);
+		_root->regions.push_back(newMBB);
 		return;
 	}
 
 	Node* current = _root;
 	while (!(current->leaf)) {
 		auto bestMBB = std::max_element(std::begin(current->regions), std::end(current->regions),
-			[this, &mbb](MBB const& a, MBB const& b) {return computeDensity(a, mbb) < computeDensity(b, mbb); });
-		expandMBB(*bestMBB, mbb);
+			[this, &newMBB](MBB const& a, MBB const& b) {return computeDensity(a, newMBB) < computeDensity(b, newMBB); });
+		expandMBB(*bestMBB, newMBB);
 		current = (Node*) (bestMBB->child);
 	}
 
 	MBB updatedMBB;
 	while (true) {
 		if (current->regions.size() < ORDER) {
-			current->regions.push_back(mbb);
+			current->regions.push_back(newMBB);
 			return;
 		}
 
-		split(*current, mbb, updatedMBB);
+		split(*current, updatedMBB, newMBB);
 
 		if (current->parent == nullptr) break;
 		
@@ -61,9 +82,9 @@ void RTree::insertHelper(MBB mbb)
 
 	Node* newRoot = new Node;
 	newRoot->regions.push_back(updatedMBB);
-	newRoot->regions.push_back(mbb);
+	newRoot->regions.push_back(newMBB);
 	Node* a = (Node*) (updatedMBB.child);
-	Node* b = (Node*) (mbb.child);
+	Node* b = (Node*) (newMBB.child);
 	a->left = b;
 	b->right = a;
 	_root = newRoot;
@@ -95,4 +116,71 @@ void RTree::expandMBB(MBB& expand, MBB const& include)
 	expand.min.y = std::min(expand.min.y, include.min.y);
 	expand.max.x = std::max(expand.max.x, include.max.x);
 	expand.max.y = std::max(expand.max.y, include.max.y);
+}
+
+void RTree::split(Node& updatedNode, MBB& updatedMBB, MBB& newMBB)
+{
+	std::vector<MBB> regions(updatedNode.regions);
+	regions.push_back(newMBB);
+
+	int seed1, seed2;
+	float minDensity = std::numeric_limits<float>::max();
+	for (int i = 0; i < (ORDER - 1); ++i) {
+		for (int j = i + 1; j < ORDER; ++j) {
+			float density = computeDensity(regions[i], regions[j]);
+			if (density < minDensity) minDensity = density, seed1 = i, seed2 = j;
+		}
+	}
+
+	updatedNode.regions.clear();
+	updatedNode.regions.push_back(regions[seed1]);
+	updatedMBB.min = updatedNode.regions.back().min;
+	updatedMBB.max = updatedNode.regions.back().max;
+
+
+	newMBB.child = new Node;
+	Node& newNode = *((Node*)(newMBB.child));
+	newNode.parent = updatedNode.parent;
+	newNode.regions.push_back(regions[seed2]);
+	newMBB.min = newNode.regions.back().min;
+	newMBB.max = newNode.regions.back().max;
+
+
+	regions[seed2] = regions.back(); regions.pop_back();
+	regions[seed1] = regions.back(); regions.pop_back();
+	
+
+	while (!(regions.empty())) {
+		float const densityU = computeDensity(updatedMBB, regions.back());
+		float const densityN = computeDensity(newMBB, regions.back());
+		if (densityU > densityN) {
+			expandMBB(updatedMBB, regions.back());
+			updatedNode.regions.push_back(regions.back());
+		}
+		else if (densityU < densityN) {
+			expandMBB(newMBB, regions.back());
+			newNode.regions.push_back(regions.back());
+
+		}
+		else {
+			if (newNode.regions.size() > updatedNode.regions.size()) {
+				expandMBB(updatedMBB, regions.back());
+				updatedNode.regions.push_back(regions.back());
+			}
+			else { 
+				expandMBB(newMBB, regions.back());
+				newNode.regions.push_back(regions.back());
+			}
+		}
+		regions.pop_back();
+	}
+
+}
+
+MBB& RTree::findChild(Node& parent, void* child)
+{
+	for (auto& mbb : parent.regions) {
+		if (mbb.child == child) return mbb;
+	}
+	std::runtime_error("COULD NOT FIND CHILD!");
 }
--- NewRTree/rtree.h
+++ NewRTree/rtree.h
@@ -8,6 +8,7 @@
 #include <math.h>
 #include <stdint.h>
 #include <limits.h>
+#include <exception>
 
 #define ORDER 3
 
@@ -34,21 +35,22 @@ public:
 	RTree();
 	~RTree();
 	void insert(Poly const& poly);
+	void print();
 	void erase(Vec2 const& vec2);
 	void forEachPoly(std::function<void(Poly const&)> const& fun);
 	void forEachMBB(std::function<void(MBB const&, int, int)> const& fun);
 	void forEachNearest(int k, Vec2 const& vec2, std::function<void(Poly const&, Vec2, Vec2, float)> const& fun);
 	size_t size() const;
-	void print();
 
 private:
+	void freeMemory(Node* current);
 	MBB buildMBB(Poly const& poly);
 	void insertHelper(MBB mbb);
 	float computeDensity(MBB const& a, MBB const& b);
 	float computeArea(float xmin, float ymin, float xmax, float ymax);
 	void expandMBB(MBB& expand, MBB const& include);
-	MBB split(Node& node, MBB& mbb, MBB& newMBB);
-	MBB& findChild(Node const& parent, void* child);
+	void split(Node& node, MBB& mbb, MBB& newMBB);
+	MBB& findChild(Node& parent, void* child);
 	void reinsertExcept(std::stack<Node*>& stack, void* except);
 	void updateParentsAfterRemoval(std::stack<Node*>& stack);
 	void getFirstIntersection(Node const& node, MBB const& mbb);
